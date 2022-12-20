7e2d4e70ff59de41574f27c8c694fb369f8aff84
eduard <eduard@cieep.com>
2022-12-20 16:22:42 +0100

    ESTE PUSH PARA JERO

---
+++ app/src/main/java/com/cieep/pmdm_c_08_retrofit_ejercicio/MainActivity.java
@@ -0,0 +1,14 @@
+package com.cieep.pmdm_c_08_retrofit_ejercicio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
